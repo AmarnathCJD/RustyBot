76cb3b4d21bd482282d2fd48a720f0ed347226e5
Amarnath C <amarnathcharichilil@gmail.com>
2023-08-19 17:22:17 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use grammers_client::{Client, Config, InitParams, Update};
 use grammers_session::Session;
 use grammers_tl_types as tl;
 use tokio::{runtime, task};
-use std::time
+use std
 
 type Result = std::result::Result<(), Box<dyn std::error::Error>>;
 
