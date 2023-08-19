0d11ac7aee9274f671dfb20aa0487d6b5743e98d
ACommiter <roseloverx@proton.me>
2023-08-19 11:38:40 +0530

    kek

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,53 @@
-use grammers_client::{Client, Config, InitParams};
+use grammers_client::{Client, Config, InitParams, Update};
 use grammers_session::Session;
 use grammers_tl_types as tl;
-use tokio::runtime;
+use tokio::{runtime, task};
 
 type Result = std::result::Result<(), Box<dyn std::error::Error>>;
+
+async fn handle_update(client: Client, update: Update) -> Result {
+    // println!("RustyBot -> Handling update: {:?}", update);
+    match update {
+        Update::NewMessage(message) if !message.outgoing() => {
+            if message.text() == "/start" {
+                handle_start_command(client, message).await?;
+            }
+        }
+        _ => {}
+    }
+
+    Ok(())
+}
+
+async fn is_chat_private(message: grammers_client::types::Message) -> bool {
+    if let grammers_client::types::Chat::User(_) = message.chat() {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+async fn handle_start_command(client: Client, message: grammers_client::types::Message) -> Result {
+    let chat = message.chat();
+
+    if is_chat_private(message).await {
+        client
+            .send_message(
+                &chat,
+                "Hello! I'm a bot written in Rust. I don't do much yet, but stay tuned!",
+            )
+            .await?;
+    } else {
+        client
+            .send_message(
+                &chat,
+                "Hello! I;m Alive :)",
+            )
+            .await?;
+    }
+
+    Ok(())
+}
 
 async fn async_main() -> Result {
     let token = "6419307556:AAGDyUmmYntQkyoY8AFljUTWjT3ARK8Y6lk";
@@ -37,13 +81,27 @@ async fn async_main() -> Result {
         println!("RustyBot -> Signed in as a bot");
     }
 
-    let chat = client.resolve_username("roseloverx")
+    let chat = client
+        .resolve_username("roseloverx")
         .await?
         .expect("chat not found");
 
     client.send_message(&chat, "Hello, world!").await?;
 
-    Ok(())
+    // Handle Updates
+
+    loop {
+        let update = client.next_update().await;
+        let upt = update?.unwrap();
+
+        let handle = client.clone();
+        task::spawn(async move {
+            match handle_update(handle, upt).await {
+                Ok(_) => {}
+                Err(e) => eprintln!("Error handling updates!: {}", e),
+            }
+        });
+    }
 }
 
 fn main() -> Result {
